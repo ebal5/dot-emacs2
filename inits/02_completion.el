ae95798dfa93dc348b3bd40f723a9b6f40bd83a2
Shuichi Tani <tani18@ss.cs.tut.ac.jp>
2018-04-25 13:53:31 +0900

    update completion and add search

--- inits/02_completion.el
+++ inits/02_completion.el
@@ -6,9 +6,9 @@
   (setq ivy-count-format "(%d/%d) ")
   :config
   (ivy-mode 1)
+  (counsel-mode 1)
   :bind
-  (("C-s" . swiper)
-   ("M-x" . counsel-M-x)
+  (("M-x" . counsel-M-x)
    ("C-x C-f" . counsel-find-file)
    ("C-x C-r" . counsel-recentf)
    ("C-c i" . counsel-imenu)
@@ -33,5 +33,4 @@
 (use-package counsel-projectile
   :config
   (counsel-projectile))
-
 ;; 02_completion.el ends here
---
+++ inits/04_search.el
@@ -0,0 +1,7 @@
+;;; Code:
+(use-package swiper
+  :bind
+  (("C-s" . swiper))
+  :config
+  (defvar swiper-include-line-number-in-search t))
+;;; 04_search.el ends here
