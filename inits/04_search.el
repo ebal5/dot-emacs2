23311ee36b0e32185a685008820621b0c938f29c
Shuichi Tani <tani18@ss.cs.tut.ac.jp>
2018-04-25 14:08:04 +0900

    Revert "update completion"
    This reverts commit b82235f4ce777b1fae03e62368c56bb691de0a80.

--- inits/04_search.el
+++ inits/04_search.el
@@ -1,7 +1,13 @@
 ;;; Code:
-(use-package swiper
-  :bind
-  (("C-s" . swiper))
+(use-package swoop
   :config
-  (defvar swiper-include-line-number-in-search t))
+  (defun isearch-forward-or-swoop-migemo (use-swoop)
+  (interactive "P")
+  (let (current-prefix-arg)
+    (call-interactively (if use-swoop 'swoop-migemo 'isearch-forward))))
+  (bind-key "C-s" 'isearch-forward-or-swoop-migemo)
+  (bind-key :map isearch-mode-map "C-o" 'swoop-from-isearch)
+  (setq swoop-minibuffer-input-dilay 0.4)
+  (setq swoop-font-size: 0.5)
+  )
 ;;; 04_search.el ends here
