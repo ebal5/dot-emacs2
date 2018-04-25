b82235f4ce777b1fae03e62368c56bb691de0a80
Shuichi Tani <tani18@ss.cs.tut.ac.jp>
2018-04-25 13:58:16 +0900

    update completion

--- inits/02_completion.el
+++ inits/02_completion.el
@@ -7,6 +7,8 @@
   :config
   (ivy-mode 1)
   (counsel-mode 1)
+  (setq ivy-use-virtual-buffers t
+	ivy-count-format "%d/%d ")  
   :bind
   (("M-x" . counsel-M-x)
    ("C-x C-f" . counsel-find-file)
