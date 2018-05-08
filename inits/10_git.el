11ef82bda967da969c9a870a33d63a60b0c0cf39
Shuichi Tani <tani18@ss.cs.tut.ac.jp>
2018-05-08 11:38:06 +0900

    :ok-hand-tone1: it works

--- inits/10_git.el
+++ inits/10_git.el
@@ -2,15 +2,16 @@
 (if (executable-find "git")
     (progn
       (use-package magit
-	:bind
-	(("C-x g" . magit-status))
-	)
+        :bind
+        (("C-x g" . magit-status))
+        )
       (use-package magit-gitflow
-	:commands (turn-on-magit-gitflow)
-	:init
-	(add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
-	)
+        :commands (turn-on-magit-gitflow)
+        :init
+        (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
+        )
       (use-package orgit)
+      (use-package gist)
       )
   )
 ;;; 10_git.el ends here
---
+++ inits/44_template.el
@@ -0,0 +1,3 @@
+;;; Code:
+(use-package yatemplate)
+;;; 44_template.el ends here
