f61d54deebfb3ecffd0fcb9a7eec1a79041a0ac5
Shuichi Tani <tani18@ss.cs.tut.ac.jp>
2018-12-05 12:40:40 +0900

    update: swoopの設定など。Migemoが動作する？

--- inits/04_search.el
+++ inits/04_search.el
@@ -1,17 +1,20 @@
 ;;; Code:
 (use-package swoop
+  :init
+  (defvar swoop-use-migemo t)
+  ;; Specify the migemo-dict place in your system.
+  (defvar swoop-migemo-options
+    "-q -e -d /usr/local/share/migemo/utf-8/migemo-dict -i \"\a\"")
+  
   :config
   (defun isearch-forward-or-swoop-migemo (use-swoop)
-  (interactive "P")
-  (let (current-prefix-arg)
-    (call-interactively (if use-swoop 'swoop-migemo 'isearch-forward))))
+    (interactive "P")
+    (let (current-prefix-arg)
+      (call-interactively (if use-swoop 'swoop-migemo 'isearch-forward))))
   (bind-key "C-s" 'isearch-forward-or-swoop-migemo)
   (bind-key "C-o" 'swoop-from-isearch isearch-mode-map)
   (bind-key "C-o" 'swoop-multi swoop-map)
   (setq swoop-minibuffer-input-dilay 0.1)
   (setq swoop-font-size: 0.5)
-  ;; Specify the migemo-dict place in your system.
-  (defvar swoop-migemo-options
-    "-q -e -d /usr/local/share/migemo/utf-8/migemo-dict")
   )
 ;;; 04_search.el ends here
